b15ba1745f0f21403d4100b11c4b0271e590cd16
Rut <rutwintzell@outlook.com>
2025-12-22 16:53:34 +0100

    Added react-router-dom

--- Rut Wintzell Portfolio/src/App.jsx
+++ Rut Wintzell Portfolio/src/App.jsx
@@ -1,5 +1,7 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
-import './App.css'
+import Home from './pages/home.jsx'
+import About from './pages/about-me.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,10 +9,22 @@ function App() {
   return (
     <>
       <div>
+
         <h1>My portfolio</h1>
         <h2>Rut Wintzell</h2>
-        <p>Blabla</p>
-        
+        <p>On this page, you can view some of my projects. 
+          Use the search bar to find projects by specific methods or technologies. 
+          If you want to learn more about me and my work, feel free to check out the 'About Me' page!</p>
+
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about-me' element={<About />} />
+          </Routes>
+        </BrowserRouter>
+
+
       </div>
     </>
   )
