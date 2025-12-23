a33f63a9d79bd74c2cf7b3a9924a9989031ae587
Rut <rutwintzell@outlook.com>
2025-12-23 10:11:05 +0100

    Small html changes

--- Rut Wintzell Portfolio/src/App.jsx
+++ Rut Wintzell Portfolio/src/App.jsx
@@ -1,20 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 import Home from './pages/home.jsx';
 import About from './pages/about-me.jsx';
+import 'index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-
-        <h1>My portfolio</h1>
-        <h2>Rut Wintzell</h2>
-        <p>On this page, you can view some of my projects. 
-          Use the search bar to find projects by specific methods or technologies. 
-          If you want to learn more about me and my work, feel free to check out the 'About Me' page!</p>
 
         <BrowserRouter>
           <Routes>
@@ -22,7 +15,6 @@ function App() {
             <Route path='/about-me' element={<About />} />
           </Routes>
         </BrowserRouter>
-
 
       </div>
     </>
--- Rut Wintzell Portfolio/src/pages/home.jsx
+++ Rut Wintzell Portfolio/src/pages/home.jsx
@@ -12,55 +12,55 @@ function Home() {
     setView(!view);
   }
 
-
   const showProjects = () => {
     changeView();
   }
 
-
   const projects = [
     {
       name: "Product Designer Portfolio",
-      description: ["This project was created to help us learn how to use Sass. It was done before I learned how to make responsive navigation, so when you zoom out, the text and images don't align properly.", " At first, I found Sass a bit challenging to understand and use correctly, but after some practice, I got the hang of it.", " I learned how to create variables, nested rules, and mixins, within Sass. Which made my CSS more organized and easier to maintain.", " Overall, this project helped me understand the many benefits of using Sass in web development. To view the full page, you can check it out on my GitHub."],
-      githubRepo: "https://github.com/rut5/Assignment-24-9-Sass",
-      image: PictureOne,
-      techArray: ["#Sass", "#CSS"]
+      /*       description: ["This project was created to help us learn how to use Sass. It was done before I learned how to make responsive navigation, so when you zoom out, the text and images don't align properly.", " At first, I found Sass a bit challenging to understand and use correctly, but after some practice, I got the hang of it.", " I learned how to create variables, nested rules, and mixins, within Sass. Which made my CSS more organized and easier to maintain.", " Overall, this project helped me understand the many benefits of using Sass in web development. To view the full page, you can check it out on my GitHub."],
+            githubRepo: "https://github.com/rut5/Assignment-24-9-Sass",
+            image: PictureOne,
+            techArray: ["#Sass", "#CSS"] */
     },
     {
       name: "Bhromaon Safe Travel",
-      description: ["This website wasn't a typical project assignment but rather a test that we had 12 hours to complete. The purpose was to experience working under a strict deadline and to learn how to distribute the workload effectively within that time frame. We were given a Figma design to recreate."
-        , " Overall, it went quite well, although I struggled a bit with aligning some parts of the content and making the page user-friendly on smaller devices."
-        , " To view the full page, you can check it out on my GitHub."],
-      githubRepo: "https://github.com/rut5/bhromaon-website",
-      image: PictureTwo,
-      techArray: ["#CSS", "#JavaScript"]
+      /*       description: ["This website wasn't a typical project assignment but rather a test that we had 12 hours to complete. The purpose was to experience working under a strict deadline and to learn how to distribute the workload effectively within that time frame. We were given a Figma design to recreate."
+              , " Overall, it went quite well, although I struggled a bit with aligning some parts of the content and making the page user-friendly on smaller devices."
+              , " To view the full page, you can check it out on my GitHub."],
+            githubRepo: "https://github.com/rut5/bhromaon-website",
+            image: PictureTwo,
+            techArray: ["#CSS", "#JavaScript"] */
     },
     {
       name: "Blue Ray Aquarium",
-      description: ["This project focused on learning how to create a responsive navigation system. I had the freedom to choose the design and the topic of the website. The website isn't fully functional yet, as I haven't added any content to the Gallery- and About Us pages, but I plan to do so later."
-        , " My goal was to let users easily find information about the different species in the aquarium, opening hours, location, and more. I also wanted the website to be simple to use and navigate on any device, which is what I used Bootstrap for."
-        , " Overall, this was a relatively easy assignment; the main challenge was finding the right components and classes for styling on the Bootstrap website."
-        , " To view the full page, you can check it out on my GitHub."],
-      githubRepo: "https://github.com/rut5/Responsive-Navigation",
-      image: PictureThree,
-      techArray: ["#Bootstrap", "#CSS"]
+      /*       description: ["This project focused on learning how to create a responsive navigation system. I had the freedom to choose the design and the topic of the website. The website isn't fully functional yet, as I haven't added any content to the Gallery- and About Us pages, but I plan to do so later."
+              , " My goal was to let users easily find information about the different species in the aquarium, opening hours, location, and more. I also wanted the website to be simple to use and navigate on any device, which is what I used Bootstrap for."
+              , " Overall, this was a relatively easy assignment; the main challenge was finding the right components and classes for styling on the Bootstrap website."
+              , " To view the full page, you can check it out on my GitHub."],
+            githubRepo: "https://github.com/rut5/Responsive-Navigation",
+            image: PictureThree,
+            techArray: ["#Bootstrap", "#CSS"] */
     }
   ]
-
 
   return (
 
     <>
-      <button className="works-btn" onClick={showProjects}>{view ? 'Hide my projects' : 'View my projects'}</button>
 
-
+      <h1>My portfolio</h1>
+      <h2>Rut Wintzell</h2>
+      <p>On this page, you can view some of my projects.
+        Use the search button to find projects by specific methods or technologies.
+        If you want to learn more about me and my work, feel free to check out the 'About Me' page!</p>
+      <button className="btn" onClick={showProjects}>{view ? 'Hide my projects' : 'View my projects'}</button>
+{/* 
       {view && (
-        <div className="w-content">
+        <div className="content">
           <h1 className="title">Some of my previous projects:</h1>
-
-
         </div>
-      )}
+      )} */}
 
       <button
         onClick={(changeView)}
