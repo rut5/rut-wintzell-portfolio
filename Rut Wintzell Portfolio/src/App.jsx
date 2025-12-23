8ed6f7b8d0236309987553d6462d8654a60766a0
Rut <rutwintzell@outlook.com>
2025-12-23 10:11:47 +0100

    Update App.jsx

--- Rut Wintzell Portfolio/src/App.jsx
+++ Rut Wintzell Portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home.jsx';
 import About from './pages/about-me.jsx';
-import 'index.css';
+import './index.css';
 
 function App() {
 
